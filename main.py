dc36d1470957b6fd134c5448ca8ebaa3d2291cf4
Mohammed Ehsan <mohammedehsan363@gmail.com>
2025-06-22 13:24:30 +0400

    infinite tic tac toe done

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import pygame
 import random
 
+from dulwich.porcelain import remove
+
 pygame.init()
 screen = pygame.display.set_mode((1920, 1080))
-table = [[0, 0, 0],
-         [0, 0, 0],
-         [0, 0, 0]]
 turn = random.randint(1, 2)
 pygame.display.set_caption('XO Showdown')
 logo = pygame.image.load('tic-tac-toe.png')
@@ -22,18 +21,32 @@ x_active = pygame.transform.scale(pygame.image.load('X active.png').convert_alph
 o_active = pygame.transform.scale(pygame.image.load('O active.png').convert_alpha(), (200, 200))
 
 board_state = [0] * 9
-print(board_state)
+
+Xorder = []
+Oorder = []
+
+
 def game_mode(turn, box):
-    
+    box_to_clear = None
     if board_state[box - 1] == 0:
         board_state[box - 1] = turn
+        if turn == 1:
+            Xorder.append(box)
+            if len(Xorder) == 4:
+                box_to_clear = Xorder[0]
+                del Xorder[0]
 
-        return 2 if turn == 1 else 1
-    return turn
+        elif turn == 2:
+            Oorder.append(box)
+            if len(Oorder) == 4:
+                box_to_clear = Oorder[0]
+                del Oorder[0]
+
+        return 2 if turn == 1 else 1, box_to_clear
+    return turn, box_to_clear
 
 
 def draw_pieces():
-
     positions = [
         (555, 95),
         (845, 95),
@@ -63,40 +76,48 @@ while run:
 
             if x >= 525 and x <= 800:
                 if y >= 65 and y <= 340:
-                    turn = game_mode(turn, 1)
+                    turn, box_to_clear = game_mode(turn, 1)
                 elif y >= 355 and y <= 630:
-                    turn = game_mode(turn, 4)
+                    turn, box_to_clear = game_mode(turn, 4)
                 elif y >= 645 and y <= 935:
-                    turn = game_mode(turn, 7)
+                    turn, box_to_clear = game_mode(turn, 7)
 
             elif x >= 815 and x <= 1090:
                 if y >= 65 and y <= 340:
-                    turn = game_mode(turn, 2)
+                    turn, box_to_clear = game_mode(turn, 2)
                 elif y >= 355 and y <= 630:
-                    turn = game_mode(turn, 5)
+                    turn, box_to_clear = game_mode(turn, 5)
                 elif y >= 645 and y <= 935:
-                    turn = game_mode(turn, 8)
+                    turn, box_to_clear = game_mode(turn, 8)
 
             elif x >= 1105 and x <= 1395:
                 if y >= 65 and y <= 340:
-                    turn = game_mode(turn, 3)
+                    turn, box_to_clear = game_mode(turn, 3)
                 elif y >= 355 and y <= 630:
-                    turn = game_mode(turn, 6)
+                    turn, box_to_clear = game_mode(turn, 6)
                 elif y >= 645 and y <= 935:
-                    turn = game_mode(turn, 9)
+                    turn, box_to_clear = game_mode(turn, 9)
+
+            if box_to_clear != None:
+                for i in range(len(board_state)):
+                    if i == box_to_clear-1:
+                        board_state[i] = 0
+
 
     if state == 'game':
         screen.fill((40, 40, 40))
         screen.blit(table_img, (510, 50))
-        screen.blit(xnon_active, (50, 30))
-        screen.blit(onon_active, (1670, 30))
 
         draw_pieces()
 
         if turn == 1:
             screen.blit(x_active, (50, 30))
+            screen.blit(onon_active, (1670, 30))
+
         elif turn == 2:
             screen.blit(o_active, (1670, 30))
+            screen.blit(xnon_active, (50, 30))
+
         else:
             print('something went wrong with the turn')
 
